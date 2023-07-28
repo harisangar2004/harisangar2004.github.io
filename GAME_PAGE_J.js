e85d3f7573eda1d7f1a04f0922aa9f50e047f505
harisangar2004 <122169178+harisangar2004@users.noreply.github.com>
2023-07-28 09:40:50 +0530

    Add files via upload

---
+++ GAME_PAGE_J.js
@@ -0,0 +1,13 @@
+function please_work_u(){
+	document.getElementById('center-box').innerHTML = '<object type = "text/html" data = "TTT1.html" style = "width : 100%; height : 100%;" ></object>';
+}
+
+
+function please_work_c(){
+	document.getElementById('center-box').innerHTML = '<object type = "text/html" data = "TTT2H.html" style = "width : 100%; height : 100%;" ></object>';
+}
+
+
+function toggleMenu() {
+  document.getElementById('sidebar').classList.toggle('active');
+}
